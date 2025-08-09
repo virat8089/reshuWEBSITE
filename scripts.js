a03fee9832a33ce8aa1eca9f1ee59ce38ac65daf
virat8089 <viratbirman@gmail.com>
2025-08-10 02:02:49 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Dark Mode Toggle
+const toggle = document.getElementById("darkToggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
